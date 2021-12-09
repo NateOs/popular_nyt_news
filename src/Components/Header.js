a999414245726d66f971865c86737218bcdd9191
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-12-09 21:38:10 +0000

    writing up the header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
+import { MdCalendarViewDay, MdSearch, MdNotes } from "react-icons/md";
 
 function Header() {
-	return (
-		<header className="header">
-			<div className="nav">
-				<span className="nav-toggle">
-					
-				</span>
-			</div>
-		</header>
-	)
+  return (
+    <header className="header container">
+      <div className="nav">
+        <span className="nav-toggle icon">
+          <MdCalendarViewDay />
+        </span>
+        <h1>New York Times Most Popular</h1>
+        <div className="toggles">
+          <span className="icon">
+            <MdSearch />
+          </span>
+          <span className="icon">
+            <MdNotes />
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Col, Row, Container } from "react-bootstrap";
 
 function Home() {
   return <div>This is the home page</div>;
