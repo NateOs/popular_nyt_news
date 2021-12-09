0d49caba847a54868bfc2e6c695d9f16e49da6e8
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-12-09 22:25:53 +0000

    building news item lists

--- src/Components/NewsListItem.js
+++ src/Components/NewsListItem.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { MdCalendarToday } from "react-icons/md";
 
 function NewsListItem() {
-  return <div>This is a News Article</div>;
+  return (
+    <div className="news-item container">
+      <img
+        className="news-item__image"
+        src="https://images.unsplash.com/photo-1639054536854-85b04c8af21e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+      />
+      <div className="news-item__title">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          quaerat et quo tenetur modi sequi.
+        </h2>
+        <p className="news-item__author">Lorem ipsum dolor sit.</p>
+        <span className="icon">
+          <MdCalendarToday /> 12/12/2020
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default NewsListItem;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import NewsListItem from "../Components/NewsListItem";
 
 function Home() {
-  return <div>This is the home page</div>;
+  return (
+    <div className="home">
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+      <NewsListItem />{" "}
+    </div>
+  );
 }
 
 export default Home;
