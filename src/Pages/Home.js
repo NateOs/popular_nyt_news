94df518642d16490b360cb43666fb2fae45e00ce
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-12-10 20:30:01 +0000

    fetching data to state

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import NewsListItem from "../Components/NewsListItem";
 
 function Home() {
+  // number of articles: last 7 or 30 days
+  let numberOfDays = 7;
+
+  // Basic state
+  const [state, setState] = useState({
+    news: [],
+    isLoading: false,
+    isError: null,
+  });
+
+  useEffect(() => {
+    const baseUrl = `https://api.nytimes.com/svc/mostpopular/v2/viewed/${numberOfDays}.json?api-key=`;
+
+    setState({ ...state, isLoading: true });
+    axios
+      .get(`${baseUrl + process.env.REACT_APP_NYTIMES_KEY}`)
+      .then((response) => {
+        console.log(response.data.results);
+        setState({
+          ...state,
+          news: response.data.results,
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, isError: error, isLoading: false });
+      });
+  }, [numberOfDays]);
+
   return (
     <div className="home">
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
-      <NewsListItem />{" "}
+      <NewsListItem /> <NewsListItem /> <NewsListItem /> <NewsListItem />{" "}
+      <NewsListItem /> <NewsListItem /> <NewsListItem /> <NewsListItem />{" "}
+      <NewsListItem /> <NewsListItem /> <NewsListItem />{" "}
     </div>
   );
 }
