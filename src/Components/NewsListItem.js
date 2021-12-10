c58b7a024158eae7b01cadf63b631000309da17c
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-12-10 22:34:34 +0000

    rendering content

--- src/Components/NewsListItem.js
+++ src/Components/NewsListItem.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { MdCalendarToday } from "react-icons/md";
 import { Link } from "react-router-dom";
+// TODO fix image link
+function NewsListItem({
+  id,
+  title,
+  media,
+  published_date: date,
+  byline: authors,
+}) {
+  // let imageUrl = media[0]["media-metadata"][0].url;
+  // console.log("imageUrl", imageUrl);
 
-function NewsListItem({ id }) {
   return (
     <Link to={`/article/${id}`}>
       <div className="news-item container">
-        <img
-          className="news-item__image"
-          src="https://images.unsplash.com/photo-1639054536854-85b04c8af21e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-        />
+        {/* {media ? (
+          <img className="news-item__image" src={imageUrl} />
+        ) : (
+          <p>no media</p>
+        )} */}
+
         <div className="news-item__title">
-          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quaerat et quo tenetur modi sequi.</h2>
-          <p className="news-item__author">Lorem ipsum dolor sit.</p>
+          <h2>{title}</h2>
+          <p className="news-item__author">{authors}</p>
           <span className="icon">
-            <MdCalendarToday /> 12/12/2020
+            <MdCalendarToday /> {date}
           </span>
         </div>
       </div>
